9f0e5d3948410e1a8bd356100645de7df5e42f58
Matthew Darby <me@mjdarby.net>
2014-10-28 23:29:15 +0000

    First few NSPL commands implemented
    enter_scene_multiple and activate now work. You will not believe how long it took to write activate correctly. It probably still doesn't work properly.

--- nspl2bf.py
+++ nspl2bf.py
@@ -28,11 +28,12 @@ class MemoryLayout:
         self.pointer = 0
         self.copy_register_offset = 0
         self.result_register_offset = 1
-        self.on_stage_one_register_offset = 2
-        self.on_stage_two_register_offset = 3
-        self.active_character_register_offset = 4
-        self.second_character_register_offset = 5
-        self.first_character_offset = 6
+        self.loop_register_offset = 2
+        self.on_stage_one_register_offset = 3
+        self.on_stage_two_register_offset = 4
+        self.active_character_register_offset = 5
+        self.second_character_register_offset = 6
+        self.first_character_offset = 7
         self.characters = []
         self.character_to_offset = {}
 
@@ -71,6 +72,12 @@ class MemoryLayout:
                   file=sys.stderr)
             raise
 
+    def move_pointer_to_offset(self, offset):
+        """Outputs the required Brainfuck commands to move to the
+        passed raw offset"""
+        self.pointer = offset
+        return ">" * offset
+
     def reset_pointer(self):
         """Returns the Brainfuck command required to move the pointer
         back to the 0 position. Needs to be called after you are
@@ -89,10 +96,12 @@ def parse_file(file_text, memory):
     while idx < len(tokens):
         current_token = tokens[idx]
         if current_token in token_function_map().keys():
-            idx += token_function_map()[current_token](tokens, memory, idx)
+            bf, off = token_function_map()[current_token](tokens, memory, idx)
+            output_brainfuck += bf
+            idx += off
         else:
             idx += 1
-    return tokens
+    return output_brainfuck
 
 def setup_memory_offsets(tokens, memory, offset):
     """Extract the character array which resides between the chars and
@@ -106,7 +115,218 @@ def setup_memory_offsets(tokens, memory, offset):
     for character in character_array:
         memory.add_character(character)
     memory.finalise_characters()
-    return 2 + len(character_array)
+    return ["", 2 + len(character_array)]
+
+def enter_characters(tokens, memory, offset):
+    """Returns the Brainfuck required to load the given characters into
+    the character registers"""
+    output_brainfuck = ""
+    character_array = extract_elements_between_tokens(
+        tokens,
+        token_pairs()["enter_scene_multiple"],
+        offset)
+    if not character_array:
+        raise Exception("No characters provided to put onto scene, aborting")
+    if len(character_array) != 2:
+        raise Exception("Wrong number of characters provided")
+    stage_offset = memory.on_stage_one_register_offset
+    for character in character_array:
+        # Move to the OS1 + OS2 registers, wipe out the current value if
+        # necessary and replace them with the index of the new characters
+        # on stage
+        output_brainfuck += memory.move_pointer_to_offset(stage_offset)
+        output_brainfuck += "[-]"
+        output_brainfuck += "+" * memory.character_to_offset[character]
+        output_brainfuck += memory.reset_pointer()
+        stage_offset += 1
+    return [output_brainfuck, 2 + len(character_array)]
+
+def activate_character(tokens, memory, offset):
+    """Returns the brainfuck for moving the given character into the
+    active character register, also moving the other character into the
+    second person register if present"""
+    output_brainfuck = ""
+    active_character = extract_next_elements(tokens, 2, offset)[1]
+    active_character_offset = memory.character_to_offset[active_character]
+    result_register_offset = memory.result_register_offset
+    active_character_register_offset = memory.active_character_register_offset
+    second_character_register_offset = memory.second_character_register_offset
+    os1_register_offset = memory.on_stage_one_register_offset
+    os2_register_offset = memory.on_stage_two_register_offset
+    copy_register_offset = memory.copy_register_offset
+    loop_register_offset = memory.loop_register_offset
+    # We'll trust that this actor is on stage, but we need to find the
+    # offset of the other actor on stage. This means checking if
+    # the first on-stage offset is equal to the active character's
+    # offset. If not, copy in the secnd on-stage offset instead.
+
+    # Reset the active character and result
+    output_brainfuck += memory.move_pointer_to_offset(result_register_offset)
+    output_brainfuck += "[-]"
+    output_brainfuck += memory.reset_pointer()
+    output_brainfuck += memory.move_pointer_to_offset(
+        active_character_register_offset)
+    output_brainfuck += "[-]"
+    output_brainfuck += memory.reset_pointer()
+
+    # Put the target active character into the result and
+    # active character slots.
+    output_brainfuck += memory.move_pointer_to_offset(result_register_offset)
+    output_brainfuck += "+" * active_character_offset
+    output_brainfuck += memory.reset_pointer()
+    output_brainfuck += memory.move_pointer_to_offset(
+        active_character_register_offset)
+    output_brainfuck += "+" * active_character_offset
+    output_brainfuck += memory.reset_pointer()
+
+    # Subtract Result from OS2, put the result in Result
+    # Sub-step: OS2 goes into copy register while subtracting
+    output_brainfuck +=  memory.move_pointer_to_offset(os2_register_offset)
+    output_brainfuck += "[-" + memory.reset_pointer()
+    output_brainfuck += memory.move_pointer_to_offset(result_register_offset)
+    output_brainfuck += "-" + memory.reset_pointer()
+    output_brainfuck += memory.move_pointer_to_offset(copy_register_offset)
+    output_brainfuck += "+" + memory.reset_pointer()
+    output_brainfuck +=  memory.move_pointer_to_offset(os2_register_offset)
+    output_brainfuck += "]" + memory.reset_pointer()
+    # Sub-step: Restore OS2
+    output_brainfuck +=  memory.move_pointer_to_offset(copy_register_offset)
+    output_brainfuck += "[-" + memory.reset_pointer()
+    output_brainfuck += memory.move_pointer_to_offset(os2_register_offset)
+    output_brainfuck += "+" + memory.reset_pointer()
+    output_brainfuck += memory.move_pointer_to_offset(copy_register_offset)
+    output_brainfuck += "]" + memory.reset_pointer()
+
+    # Set Sec to 0. If Result != 0, add 1 to Sec. Reset Result.
+    output_brainfuck += memory.move_pointer_to_offset(
+        second_character_register_offset)
+    output_brainfuck += "[-]" + memory.reset_pointer()
+    output_brainfuck += memory.move_pointer_to_offset(result_register_offset)
+    output_brainfuck += "[" + memory.reset_pointer()
+    output_brainfuck += memory.move_pointer_to_offset(
+        second_character_register_offset)
+    output_brainfuck += "+" + memory.reset_pointer()
+    # Reset result.
+    output_brainfuck += memory.move_pointer_to_offset(result_register_offset)
+    output_brainfuck += "[-]"
+    output_brainfuck += "]" + memory.reset_pointer()
+
+
+    # If Sec != 0, copy OS2 into Result
+    output_brainfuck += memory.move_pointer_to_offset(
+        second_character_register_offset)
+    output_brainfuck += "[" + memory.reset_pointer()
+    # Sub-step: OS2 goes into copy register while adding
+    output_brainfuck +=  memory.move_pointer_to_offset(os2_register_offset)
+    output_brainfuck += "[-" + memory.reset_pointer()
+    output_brainfuck += memory.move_pointer_to_offset(result_register_offset)
+    output_brainfuck += "+" + memory.reset_pointer()
+    output_brainfuck += memory.move_pointer_to_offset(copy_register_offset)
+    output_brainfuck += "+" + memory.reset_pointer()
+    output_brainfuck +=  memory.move_pointer_to_offset(os2_register_offset)
+    output_brainfuck += "]" + memory.reset_pointer()
+    # Sub-step: Restore OS2
+    output_brainfuck +=  memory.move_pointer_to_offset(copy_register_offset)
+    output_brainfuck += "[-" + memory.reset_pointer()
+    output_brainfuck += memory.move_pointer_to_offset(os2_register_offset)
+    output_brainfuck += "+" + memory.reset_pointer()
+    output_brainfuck += memory.move_pointer_to_offset(copy_register_offset)
+    output_brainfuck += "]" + memory.reset_pointer()
+    output_brainfuck += memory.move_pointer_to_offset(
+        loop_register_offset)
+    output_brainfuck += "+" + memory.reset_pointer()
+    output_brainfuck += memory.move_pointer_to_offset(
+        second_character_register_offset)
+    output_brainfuck += "-"
+    output_brainfuck += "]" + memory.reset_pointer()
+
+    # Restore Sec from Loop
+    output_brainfuck +=  memory.move_pointer_to_offset(loop_register_offset)
+    output_brainfuck += "[-" + memory.reset_pointer()
+    output_brainfuck += memory.move_pointer_to_offset(
+        second_character_register_offset)
+    output_brainfuck += "+" + memory.reset_pointer()
+    output_brainfuck += memory.move_pointer_to_offset(loop_register_offset)
+    output_brainfuck += "]" + memory.reset_pointer()
+
+    # Sec - 1
+    output_brainfuck += memory.move_pointer_to_offset(
+        second_character_register_offset)
+    output_brainfuck += "-" + memory.reset_pointer()
+
+    # If Sec != 0, reset sec, copy OS1 into Sec
+    output_brainfuck += memory.move_pointer_to_offset(
+        second_character_register_offset)
+    output_brainfuck += "[[-]" + memory.reset_pointer()
+    # Sub-step: OS1 goes into copy register while adding to Sec
+    output_brainfuck +=  memory.move_pointer_to_offset(os1_register_offset)
+    output_brainfuck += "[-" + memory.reset_pointer()
+    output_brainfuck += memory.move_pointer_to_offset(
+        second_character_register_offset)
+    output_brainfuck += "+" + memory.reset_pointer()
+    output_brainfuck += memory.move_pointer_to_offset(copy_register_offset)
+    output_brainfuck += "+" + memory.reset_pointer()
+    output_brainfuck +=  memory.move_pointer_to_offset(os1_register_offset)
+    output_brainfuck += "]" + memory.reset_pointer()
+    # Sub-step: Restore OS1
+    output_brainfuck +=  memory.move_pointer_to_offset(copy_register_offset)
+    output_brainfuck += "[-" + memory.reset_pointer()
+    output_brainfuck += memory.move_pointer_to_offset(os1_register_offset)
+    output_brainfuck += "+" + memory.reset_pointer()
+    output_brainfuck += memory.move_pointer_to_offset(copy_register_offset)
+    output_brainfuck += "]" + memory.reset_pointer()
+    output_brainfuck += memory.move_pointer_to_offset(
+        second_character_register_offset)
+    output_brainfuck += "[-" + memory.reset_pointer()
+    output_brainfuck += memory.move_pointer_to_offset(
+        loop_register_offset)
+    output_brainfuck += "+" + memory.reset_pointer()
+    output_brainfuck += memory.move_pointer_to_offset(
+        second_character_register_offset)
+    output_brainfuck += "]" + memory.reset_pointer()
+    output_brainfuck += memory.move_pointer_to_offset(
+        second_character_register_offset)
+    output_brainfuck += "]" + memory.reset_pointer()
+
+    # Restore Sec
+    output_brainfuck += memory.move_pointer_to_offset(
+        loop_register_offset)
+    output_brainfuck += "[-" + memory.reset_pointer()
+    output_brainfuck += memory.move_pointer_to_offset(
+        second_character_register_offset)
+    output_brainfuck += "+" + memory.reset_pointer()
+    output_brainfuck += memory.move_pointer_to_offset(
+        loop_register_offset)
+    output_brainfuck += "]" + memory.reset_pointer()
+
+    # Add result to Sec.
+    # Sub-step: Result goes into copy register while adding to Sec
+    output_brainfuck +=  memory.move_pointer_to_offset(result_register_offset)
+    output_brainfuck += "[-" + memory.reset_pointer()
+    output_brainfuck += memory.move_pointer_to_offset(
+        second_character_register_offset)
+    output_brainfuck += "+" + memory.reset_pointer()
+    output_brainfuck += memory.move_pointer_to_offset(copy_register_offset)
+    output_brainfuck += "+" + memory.reset_pointer()
+    output_brainfuck +=  memory.move_pointer_to_offset(result_register_offset)
+    output_brainfuck += "]" + memory.reset_pointer()
+    # Sub-step: Restore OS1
+    output_brainfuck +=  memory.move_pointer_to_offset(copy_register_offset)
+    output_brainfuck += "[-" + memory.reset_pointer()
+    output_brainfuck += memory.move_pointer_to_offset(result_register_offset)
+    output_brainfuck += "+" + memory.reset_pointer()
+    output_brainfuck += memory.move_pointer_to_offset(copy_register_offset)
+    output_brainfuck += "]" + memory.reset_pointer()
+
+    # Reset result
+    output_brainfuck += memory.move_pointer_to_offset(result_register_offset)
+    output_brainfuck += "[-]" + memory.reset_pointer()
+    return [output_brainfuck, 2]
+
+def extract_next_elements(tokens, number_of_elements, offset):
+    """Starting from the offset element of the tokens array, extract the
+    next N elements."""
+    return tokens[offset:offset + number_of_elements]
 
 def extract_elements_between_tokens(tokens, token_pair, offset):
     """Starting from the offset element of the tokens array, find all
@@ -123,11 +343,15 @@ def extract_elements_between_tokens(tokens, token_pair, offset):
     return elements.split(",")
 
 def token_function_map():
-    function_map = {"chars": setup_memory_offsets}
+    function_map = {"chars": setup_memory_offsets,
+                    "enter_scene_multiple": enter_characters,
+                    "activate": activate_character}
     return function_map
 
 def token_pairs():
-    pairs_map = {"chars": ["chars", "endchars"]}
+    pairs_map = {"chars": ["chars", "endchars"],
+                 "enter_scene_multiple": ["enter_scene_multiple",
+                                          "end_enter_scene_multiple"]}
     return pairs_map
 
 if __name__ == "__main__":
@@ -146,4 +370,5 @@ if __name__ == "__main__":
     text = re.sub(', *$', '', text)
     f.close()
 
-    tokens = parse_file(text, mem)
+    brainfuck = parse_file(text, mem)
+    print(brainfuck)
